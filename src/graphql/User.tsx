d132b7f405e90a1e4b1f030f309e7146bac1298a
Turuani Alois <Alois.Turuani@plansee-group.com>
2023-02-17 16:38:05 +0000

    wip

--- src/graphql/User.tsx
+++ src/graphql/User.tsx
@@ -13,18 +13,18 @@ const QUERY_ALL_USERS = gql`
 `;
 
 export function UserInfo() {
-    const { data, loading } = useQuery(
+    const { data, loading, error } = useQuery(
         QUERY_ALL_USERS, {
         pollInterval: 500
     }
     );
 
     if (loading) return <p>Loading ...</p>
-
-    return data.users.map(({ id, username, email }) => (
-        <div key={id}>
+    if (error) return `Error! ${error.message}`
+    return data.users.map((user: { id: number; username: string; email: string; }) => (
+        <div key={user.id}>
             <p>
-                User - {id}: {username} {email}
+                User - {user.id}: {user.username} {user.email}
             </p>
         </div>
     ))
